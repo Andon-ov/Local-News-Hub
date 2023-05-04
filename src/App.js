42aa21fb855628e5ec2932f2f22bea3a37e0ebee
BloodTaint <Bloodtaint@yahoo.com>
2023-05-04 07:59:43 +0300

    start from scratch with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
-// import Brand from './componentsOld/Brand';
-// import Footer from './componentsOld/Footer';
-// import TopBar from './componentsOld/TopBar';
-// import NavBar from './componentsOld/NavBar';
-// import TopNews from './componentsOld/TopNews';
-// import CatNews from './componentsOld/CatNews';
-// import TabNews from './componentsOld/TabNews';
-// import MainNews from './componentsOld/MainNews';
-// import ContactUs from './componentsOld/ContactUs';
-// import SinglePage from './componentsOld/SinglePage';
+// react Bootstrap  CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-/* <TopBar />
+import './App.css';
 
-<Brand />
-
-<NavBar />
-
-<TopNews />
-
-<CatNews />
-
-<TabNews />
-
-<MainNews />
-
-<ContactUs />
-
-<SinglePage />
-
-<Footer />
-
-<a href="http://our-recipes.ml/" className="back-to-top"><i className="fa fa-chevron-up"></i></a> */
-
-
-
-
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
+import Carousel from './components/Carousel';
+import Nav from './components/Nav';
+import TabsNav from './components/TabsNav';
+import Card from './components/Card';
 
 function App() {
-    return (
-        <>
-            <Header/>
-            <Main/>
-            <Footer/>
-
-        </>
-    );
+  return (
+    <>
+      <header className="header">
+        <Nav />
+      </header>
+      <main className="main">
+        <section className="hero">
+          <Carousel />
+          <TabsNav />
+        </section>
+        <section className="cards">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </section>
+      </main>
+      <footer className="footer"></footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import Card from 'react-bootstrap/Card';
+
+function ImgOverlayExample() {
+    return (
+        <Card className="bg-dark text-white">
+            <Card.Img src="https://radio999bg.com/images/radio999bg/cache/news/ms-otmeni-natsionalnata-zabrana-za-vnos-ukrainski-stoki-960x640.jpg" alt="Card image" />
+            <Card.ImgOverlay>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in
+                    to additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+        </Card>
+    );
+}
+
+export default ImgOverlayExample;
